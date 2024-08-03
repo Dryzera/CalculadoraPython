26cdd45cc14f0425d6648e3d23a15045c6e7b718
Dryzera <endryusdaniel6321@gmail.com>
2024-08-03 17:12:18 -0300

    solucionando qss de botoes

--- src/buttons.py
+++ src/buttons.py
@@ -5,6 +5,8 @@ from utils import isNumOrDot, isEmpty, isValidNumber
 from display import Display
 from variables import MEDIUM_FONT_SIZE
 from typing import TYPE_CHECKING
+from styles import qss
+from variables import DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR
 
 if TYPE_CHECKING:
     from display import Display
@@ -13,7 +15,7 @@ if TYPE_CHECKING:
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
-        super().__init__()
+        super().__init__(*args, **kwargs)
         self.configStyle()
     
     def configStyle(self):
@@ -21,7 +23,6 @@ class Button(QPushButton):
         font.setPixelSize(MEDIUM_FONT_SIZE)
         self.setFont(font)
         self.setMinimumSize(75, 75)
-        self.setProperty('cssClass', 'specialButton')
 
 class ButtonsGrid(QGridLayout):
     def __init__(self, display: 'Display', info: 'Info', *args, **kwargs) -> None:
@@ -54,8 +55,9 @@ class ButtonsGrid(QGridLayout):
         for i, row in enumerate(self._grid_mask):
             for j, button_text in enumerate(row):
                 button = Button(button_text)
-                if not isNumOrDot(button_text) and isEmpty(button_text):
-                    button.setProperty('cssClass', 'specialButton')
+                if not isNumOrDot(button_text) and not isEmpty(button_text):
+                    button.setStyleSheet(f'background-color: {DARKER_PRIMARY_COLOR}')
+                    print(button_text, 2222222222222)
 
                 self.addWidget(button, i, j)
                 buttonSlot = self._makeButtonDisplay(
@@ -78,4 +80,3 @@ class ButtonsGrid(QGridLayout):
         def realSlot():
             func(*args, **kwargs)
         return realSlot
-
